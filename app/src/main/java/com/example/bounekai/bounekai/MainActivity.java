17ca1c3fe35bea8d13b84172f550e300ccddbda6
Airi Nakashima <nakanaka.shimshima@gmail.com>
2018-11-20 01:39:27 +0900

    画面

--- app/src/main/java/com/example/bounekai/bounekai/MainActivity.java
+++ app/src/main/java/com/example/bounekai/bounekai/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.bounekai.bounekai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button lottery=findViewById(R.id.ChanceLottery);
+        lottery.setOnClickListener(new View.OnClickListener(){
+            /** ボタンをクリックした時に呼ばれる */
+            @Override
+            public void onClick(View v) {
+                String a="fd";
+                TextView winNum1=findViewById(R.id.winNum1);
+                winNum1.setText(a);
+                TextView winNum2=findViewById(R.id.winNum2);
+                winNum2.setText(a);
+                TextView winNum3=findViewById(R.id.winNum3);
+                winNum3.setText(a);
+
+            }
+        });
     }
-}
+        }
+
