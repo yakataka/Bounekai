41005888998af9c3ed1dd74785c74ab6121d13e5
hajime1046 <hajime1046@users.noreply.github.com>
2018-11-24 17:32:56 +0900

    update処理追加(途中)

--- app/src/main/java/com/example/bounekai/bounekai/LotteryActivity.java
+++ app/src/main/java/com/example/bounekai/bounekai/LotteryActivity.java
@@ -1,5 +1,6 @@
 package com.example.bounekai.bounekai;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -24,20 +25,25 @@ public class LotteryActivity extends AppCompatActivity {
 
         // 抽選対象取得
         rosterDtoList = selectLotteryTarget();
-//        for(int i = 0; i < rosterDtoList.size(); i++) {
-//            Log.v("num", rosterDtoList.get(i).getSyainNum());
-//        }
+        for(int i = 0; i < rosterDtoList.size(); i++) {
+            Log.v("num", rosterDtoList.get(i).getSyainNum() + " " + rosterDtoList.get(i).getHitFlg());
+        }
         // シャッフル
         Collections.shuffle(rosterDtoList);
 
         Intent intent = getIntent();
         int lottery_times = intent.getIntExtra("LOTTERY_TIMES", 0);
+        String[] hit_syain_num = new String[lottery_times];
         for (int i = 0; i < lottery_times; i++) {
             result = result + rosterDtoList.get(i).getSyainNum() + "  ";
+            hit_syain_num[i] = rosterDtoList.get(i).getSyainNum();
         }
 
         TextView lotteryResult = findViewById(R.id.lottery_result);
         lotteryResult.setText(result);
+
+        // 当選フラグ更新
+        updateLotteryTarget(hit_syain_num);
     }
 
     private ArrayList<RosterDto> selectLotteryTarget() {
@@ -60,6 +66,7 @@ public class LotteryActivity extends AppCompatActivity {
                 RosterDto rosterDto = new RosterDto();
                 rosterDto.setSyainNum(cursor.getString(3));
                 rosterDto.setLotNum(cursor.getString(4));
+                rosterDto.setHitFlg(cursor.getInt(5));
                 rosterDtoList.add(rosterDto);
             }
         } finally {
@@ -68,28 +75,21 @@ public class LotteryActivity extends AppCompatActivity {
         return rosterDtoList;
     }
 
-    private ArrayList<RosterDto> updateLotteryTarget() {
+    private void updateLotteryTarget(String[] hit_syain_num) {
         SQLiteDatabase db = openHelper.getWritableDatabase();
-//        try {
-//            Cursor cursor;
-//            cursor = db.update(
-//                    WordContract.Words.TABLE_NAME,
-//                    null,
-//                    null,
-//                    null
-//            );
-//            int numRows = cursor.getCount();
-//
-//            cursor.moveToFirst();
-//            for (int i = 0; i < numRows; i++, cursor.moveToNext()) {
-//                RosterDto rosterDto = new RosterDto();
-//                rosterDto.setSyainNum(cursor.getString(3));
-//                rosterDto.setLotNum(cursor.getString(4));
-//                rosterDtoList.add(rosterDto);
-//            }
-//        } finally {
-//            db.close();
-//        }
-        return rosterDtoList;
+        try {
+            Cursor cursor;
+            ContentValues cv = new ContentValues();
+            // put(変更するカラム, 変更する値)
+            cv.put("hit_flg", 1);
+            for (int i = 0; i < hit_syain_num.length; i++) {
+                db.update(WordContract.Words.TABLE_NAME,
+                        cv,
+                        "syain_num = ?",
+                        new String[]{hit_syain_num[i]});
+            }
+        } finally {
+            db.close();
+        }
     }
 }
